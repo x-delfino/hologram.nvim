8f40ba1bf612a96b103d622f63848f41c7212a94
aled <aled.mehta@withsecure.com>
2023-03-10 03:23:52 +0000

    wip

--- lua/hologram/terminal.lua
+++ lua/hologram/terminal.lua
@@ -1,4 +1,6 @@
 local base64 = require('hologram.base64')
+local socket = require("socket")
+local log = require('hologram.log')
 
 local terminal = {}
 
@@ -48,6 +50,37 @@ local CTRL_KEYS = {
     -- TODO: Animation
 }
 
+function terminal._table_invert(table)
+   local inverted={}
+   for k,v in pairs(table) do
+     inverted[v]=k
+   end
+   return inverted
+end
+
+
+function terminal._parse_graphics_response(resp)
+    if resp:match('\x1b_G.*\x1b\\') then
+	local delim = resp:find(';')
+	local message = resp:sub(delim+1, -3)
+	local keyref = terminal._table_invert(CTRL_KEYS)
+	local keystring = resp:sub(5, delim)
+	local keys = {}
+	-- split by comma (this needs cleaning up)
+	for key in keystring:gmatch('([^,]+)') do
+	    local keydelim = key:find('=')
+	    local k = key:sub(1,keydelim-1)
+	    local ref = keyref[k]
+	    if ref then k = ref end
+	    keys[k] = key:sub(keydelim+1)
+        end
+	log.debug(keys)
+	log.info('message: ' .. message)
+	return message, keys
+    else return nil end
+end
+
+
 function terminal.send_graphics_command(keys, payload)
     if payload and string.len(payload) > 4096 then keys.more = 1 else keys.more = 0 end
     local ctrl = ''
@@ -57,21 +90,17 @@ function terminal.send_graphics_command(keys, payload)
         end
     end
     ctrl = ctrl:sub(0, -2) -- chop trailing comma
-
     if payload then
---        if keys.transmission_type ~= 'd' then
-            payload = base64.encode(payload)
---        end
+        payload = base64.encode(payload)
         payload = terminal.get_chunked(payload)
 	local encoded_payload = ''
         for i=1,#payload do
-	    -- terminal.write('\x1b_G'..ctrl..';'..payload[i]..'\x1b\\')
 	    encoded_payload = encoded_payload .. '\x1b_G'..ctrl..';'..payload[i]..'\x1b\\'
             if i == #payload-1 then ctrl = 'm=0' else ctrl = 'm=1' end
         end
-        terminal.write(encoded_payload)
+        terminal.readwrite(encoded_payload)
     else
-        terminal.write('\x1b_G'..ctrl..'\x1b\\')
+        terminal.readwrite('\x1b_G'..ctrl..'\x1b\\')
     end
 end
 
@@ -82,7 +111,7 @@ function terminal.get_chunked(str)
     local cap = 4096
     for i = 1,#str,4096 do
     	if ((#str - i) + 1) < 4096 then
-	    cap = #str - i
+	    cap = #str
         end
 --        local chunk = str:sub(i, i + cap - 1):gsub('%s', '') -- the gsub segfaulted
         local chunk = str:sub(i, i + cap - 1)
@@ -102,19 +131,29 @@ function terminal.restore_cursor()
     terminal.write('\x1b[u')
 end
 
--- glob together writes to stdout
 terminal.write = vim.schedule_wrap(function(data)
-    local stdin = vim.loop.new_tty(0, true)
-    local stdout = vim.loop.new_tty(1, false)
-    stdout:write(data)
-    stdin:read_start(function (err, data)
-        assert(not err, err)
-        if data then
-        else
-          stdin:close()
-          stdout:close()
+    io.stdout:write(data)
+    io.stdout:flush()
+end)
+
+-- glob together writes to stdout
+terminal.readwrite = vim.schedule_wrap(function(data)
+    io.stdout:write(data)
+    io.stdout:flush()
+    --for milliseconds
+    local time = socket.gettime()*1000
+    --read response
+    local resp = " "
+    repeat
+	local read = io.stdin:read(1)
+	if read then
+	    if resp then
+                resp = resp .. read
+	    else resp = read end 
         end
-    end)
+    --stop reading input when end of reply with 0.3s timeout
+    until resp:match('.*\x1b\\') or (socket.gettime()*1000 - time) > 300
+    return terminal._parse_graphics_response(resp)
 end)
 
 return terminal
